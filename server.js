bef7f7a90629b459b015a2d80f6fb1a8b379eed5
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-03-22 21:23:07 -0500

    Chagenlle 2 change 1

--- server.js
+++ server.js
@@ -6,13 +6,21 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // Para pruebas de FCC
+
+// 1. MIDDLEWARES (Usa app.use)
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+// 2. CONFIGURACIÓN DE PUG
+app.set('view engine', 'pug');
+app.set('views', './views/pug'); 
 
+// 3. RUTAS
+app.route('/').get((req, res) => {
+  // Renderiza el archivo index.pug
+  res.render('index'); 
 });
 
 const PORT = process.env.PORT || 3000;
